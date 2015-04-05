bc9f0a3a1a632ca1a7cfc83a435c7703f7936e93
CMP15group2 <aya_monem92@yahoo.com>
2015-04-05 05:51:24 -0700

    "Fire Feature"

--- Project Source Files/Actions.h
+++ Project Source Files/Actions.h
@@ -2,10 +2,16 @@
 #include <iostream>
 using namespace std;
 
-void Run (supermario &man)
+void Run (supermario &man,Enemy &Enemy)
 {cout << "Run fn is called for the supermario at ( " << man.pos.x << " , " << man.pos.y << " )\n";
 
 	// constraints not handled
 	man.pos.x += 1;
 	man.pos.y += 1;
+
+	//kill
+	man.points  -=1;
+    Enemy.power -=1;
+    man.health -=1;
+
 }
--- Project Source Files/main.cpp
+++ Project Source Files/main.cpp
@@ -1,10 +1,12 @@
 #include "Actions.h"
+#include "Kill.h"
 int main ()
 {
-	supermario player1;
-	while(1)
-	{
-		Run(player1);
-	}
-	return 0;
+supermario player1;
+Enemy E1;
+while(1)
+{
+Run (player1,E1);
+}
+return 0;
 }
